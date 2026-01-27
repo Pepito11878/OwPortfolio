1703830f74b4b7512aa1a9713919c2c004f9ba28
Lucas Q <lucas.queveau44190@gmail.com>
2026-01-27 11:05:05 +0100

    test profile picture

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
-
 export default function App() {
   return (
     <>
-      <div></div>
+      <main>
+        <img className="size-[260px] rounded-full" src="/images/profile-picture.jpg" alt="photo de profil" />
+        <h1>Pepito11 - dps player</h1>
+        <p>future description</p>
+      </main>
     </>
-  )
+  );
 }
-
