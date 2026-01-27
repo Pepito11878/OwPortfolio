b84180982e41f95bd5a04a1896007e56c9cd39ea
Lucas Q <lucas.queveau44190@gmail.com>
2026-01-27 10:26:59 +0100

    chore add header and footer to main.tsx

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,13 +1,17 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
-import './index.css'
-import App from './App.tsx'
+import "./index.css";
+import App from "./App.tsx";
+import { Header } from "./components/Header.tsx";
+import { Footer } from "./components/Footer.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/Contact" element={<App />} />
     </Routes>
+    <Footer />
   </BrowserRouter>,
-)
+);
